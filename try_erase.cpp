2565cd353cc2ba634345930f0ca5c0b44a798542
He Zhang <hxzhang1@ualr.edu>
2017-12-27 23:12:17 -0600

    no idea why vector<A> can erase iterator out of range,

--- try_erase.cpp
+++ try_erase.cpp
@@ -3,7 +3,36 @@
 
 using namespace std; 
 
+class A{
+public:
+    A(){}
+    double a;
+    int b; 
+};
+
+void test1();
+void test0();
+
 int main()
+{
+    test1();
+    return 1; 
+}
+
+
+void test1()
+{
+    vector<A> vA; 
+    A a1; A a2; 
+    vA.push_back(a1); 
+    vA.push_back(a2);
+    cout <<" vA size: "<<vA.size()<<endl; 
+    vA.erase(vA.begin() + 10); 
+    cout <<"vA size: "<<vA.size()<<endl;
+    return ; 
+}
+
+void test0()
 {
     vector<int> va{1, 2, 3, 5, 7}; 
     vector<int>::iterator ita = va.begin() + va.size(); 
@@ -32,5 +61,12 @@ int main()
     {
 	cout <<" it = va1.end() "<<endl;
     }
-    return 1;
+
+    vector<int> va2{1, 2, 3};
+    cout <<"va2 size: "<<va2.size()<<endl; 
+    va2.erase(va2.begin() + 2); 
+    cout <<"va2 size: "<<va2.size()<<endl;
+
+    return ;
 }
+
