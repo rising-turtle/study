fcddb8bb550f4992e9b2dc3686792221fe5cefca
He <hxzhang1@ualr.edu>
2019-05-07 22:47:33 -0400

    add try_round

---
+++ try_round.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std; 
+
+int main()
+{
+    float a = -2.1; 
+    cout <<"a "<<a<<" floor(a): "<<floor(a)<<" ceil(a): "<<ceil(a)<<" round(a): "<<round(a)<<endl;
+    a = -2.8; 
+    cout <<"a "<<a<<" floor(a): "<<floor(a)<<" ceil(a): "<<ceil(a)<<" round(a): "<<round(a)<<endl;
+    a = 2.1; 
+    cout <<"a "<<a<<" floor(a): "<<floor(a)<<" ceil(a): "<<ceil(a)<<" round(a): "<<round(a)<<endl;
+    a = 2.8;
+    cout <<"a "<<a<<" floor(a): "<<floor(a)<<" ceil(a): "<<ceil(a)<<" round(a): "<<round(a)<<endl;
+    return 0;
+
+}
